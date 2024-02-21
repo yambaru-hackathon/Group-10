e086e7072054bc0ace57ec84b3b706e60f3875f6
nishida-kota <anknow4649@gmail.com>
2024-02-22 00:57:33 +0900

    テスト

--- lib/document_list.dart
+++ lib/document_list.dart
@@ -1,1 +1,14 @@
-// test //
+import 'package:flutter/material.dart';
+
+class Import extends StatelessWidget {
+  const Import({super.key});
+
+    @override
+      Widget build(context) {
+        return Scaffold(
+            appBar: AppBar(title: const Text('import')),
+            body: const Center(
+                child: Text('Hello World'),
+            ));
+    }
+}
